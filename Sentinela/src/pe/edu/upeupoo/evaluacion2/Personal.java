dce302100e39c1076bcf03d1dc9f53aaa63bd8bc
Alumnos <Alumnos@LC01-PC23>
2016-01-22 11:36:40 -0500

    evaluacion

--- Sentinela/src/pe/edu/upeupoo/evaluacion2/Personal.java
+++ Sentinela/src/pe/edu/upeupoo/evaluacion2/Personal.java
@@ -2,4 +2,6 @@ package pe.edu.upeupoo.evaluacion2;
 
 public class Personal {
 
-}
+	private Datosgenerales datosgenerales;
+	private Profesion profesion;
+	private Cargo cars=new Cargo();
--- Sentinela/src/pe/edu/upeupoo/evaluacion2/Universidad.java
+++ Sentinela/src/pe/edu/upeupoo/evaluacion2/Universidad.java
@@ -2,4 +2,47 @@ package pe.edu.upeupoo.evaluacion2;
 
 public class Universidad {
 
+	private String upeu;
+	private String direccion;
+	private String email;
+	private String celular;
+	private String redes;
+	private String web;
+	public String getUpeu() {
+		return upeu;
+	}
+	public void setUpeu(String upeu) {
+		this.upeu = upeu;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getCelular() {
+		return celular;
+	}
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+	public String getRedes() {
+		return redes;
+	}
+	public void setRedes(String redes) {
+		this.redes = redes;
+	}
+	public String getWeb() {
+		return web;
+	}
+	public void setWeb(String web) {
+		this.web = web;
+	}
+	
 }
